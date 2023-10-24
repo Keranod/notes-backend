c186fe9a715a5061111b4f107e82d0f4f3c12902
Keranod <konrad.konkel@wp.pl>
2023-10-24 09:04:46 +0100

    test run cannot connect to db debugging; take3

--- tests/notes_api.test.js
+++ tests/notes_api.test.js
@@ -9,7 +9,7 @@ test('notes are returned as json', async () => {
     .get('/api/notes')
     .expect(200)
     .expect('Content-Type', /application\/json/)
-}, 100000)
+}, 10000)
 
 afterAll(async () => {
   await mongoose.connection.close()
