09cf2fb7a2ca7b3f9b075b707622ee1443aeb2f8
Keranod <konrad.konkel@wp.pl>
2023-10-24 13:06:38 +0100

    Eliminating the try-catch

--- controllers/notes.js
+++ controllers/notes.js
@@ -19,41 +19,30 @@ notesRouter.get('/:id', (request, response, next) => {
 })
 
 // eslint-disable-next-line no-unused-vars
-notesRouter.post('/', async (request, response, next) => {
+notesRouter.post('/', async (request, response) => {
     const body = request.body
 
     const note = new Note({
         content: body.content,
         important: body.important || false,
     })
-    try {
-        const savedNote = await note.save()
-        response.status(201).json(savedNote)
-    } catch(exception) {
-        next(exception)
+
+    const savedNote = await note.save()
+    response.status(201).json(savedNote)
+})
+
+notesRouter.get('/:id', async (request, response) => {
+    const note = await Note.findById(request.params.id)
+    if (note) {
+        response.json(note)
+    } else {
+        response.status(404).end()
     }
 })
 
-notesRouter.get('/:id', async (request, response, next) => {
-    try {
-        const note = await Note.findById(request.params.id)
-        if (note) {
-            response.json(note)
-        } else {
-            response.status(404).end()
-        }
-    } catch(exception) {
-        next(exception)
-    }
-})
-
-notesRouter.delete('/:id', async (request, response, next) => {
-    try {
-        await Note.findByIdAndRemove(request.params.id)
-        response.status(204).end()
-    } catch(exception) {
-        next(exception)
-    }
+notesRouter.delete('/:id', async (request, response) => {
+    await Note.findByIdAndRemove(request.params.id)
+    response.status(204).end()
 })
 
 module.exports = notesRouter
